e15855a6c577d080d30f5a5de1fb44f34bcfd6a2
nagahama <nghm7980@gmail.com>
2026-04-23 17:38:12 +0900

    5. 2 Usersリソース

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2026_04_20_054723) do
-  create_table "users", charset: "latin1", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
