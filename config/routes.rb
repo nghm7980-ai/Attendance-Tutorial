1bccfe199251dedaddd0504f7c3906f217d8b2ea
nagahama <nghm7980@gmail.com>
2026-04-27 17:17:35 +0900

    Finish login-function

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # ログイン機能
   get    '/login', to: 'sessions#new'
   post   '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-    
+  get '/logout', to: 'sessions#destroy'
+
   resources :users
 end
