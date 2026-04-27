d06e303babafada28992c6a04aaaeb04e5dcac24
nagahama <nghm7980@gmail.com>
2026-04-27 18:51:56 +0900

    7. 2 ふたつの小さなバグを直そう

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_04_20_054723) do
+ActiveRecord::Schema[7.1].define(version: 2026_04_27_085815) do
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
