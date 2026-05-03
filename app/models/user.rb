2d0bc62fed4fd40cbd178491f97f8ae9c17e9654
nagahama <nghm7980@gmail.com>
2026-05-03 09:47:07 +0900

    9. 2 基本情報となるカラム

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,10 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true    
   validates :department, length: { in: 2..30 }, allow_blank: true
+  validates :basic_time, presence: true
+  validates :work_time, presence: true 
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   # 渡された文字列のハッシュ値を返します。
   def User.digest(string)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_04_29_035426) do
+ActiveRecord::Schema[7.1].define(version: 2026_05_03_002249) do
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.1].define(version: 2026_04_29_035426) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
+    t.datetime "basic_time", default: "2026-05-02 23:00:00"
+    t.datetime "work_time", default: "2026-05-02 22:30:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
