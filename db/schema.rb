ba1623393f943a621ff7bcfef3cc5251a568ceea
nagahama <nghm7980@gmail.com>
2026-05-03 09:14:53 +0900

    9. 1 「department（所属）」カラム

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_04_28_052117) do
+ActiveRecord::Schema[7.1].define(version: 2026_04_29_035426) do
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_04_28_052117) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.string "department"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
