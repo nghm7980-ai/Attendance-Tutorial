07bfbc96f9a3918dbb2765f9e6ea10141a4dc7eb
nagahama <nghm7980@gmail.com>
2026-05-06 10:08:46 +0900

    10. 6 退勤登録ボタンを作ろう

--- db/schema.rb
+++ db/schema.rb
@@ -31,8 +31,8 @@ ActiveRecord::Schema[7.1].define(version: 2026_05_04_053801) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2026-05-02 23:00:00"
-    t.datetime "work_time", default: "2026-05-02 22:30:00"
+    t.datetime "basic_time", default: "2026-05-05 23:00:00"
+    t.datetime "work_time", default: "2026-05-05 22:30:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
